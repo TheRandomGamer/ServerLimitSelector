8a8d35e35b9de52083dfee6648bb7ec457c8af1b
TheRandomGamerMC <unconfigured@null.spigotmc.org>
2017-05-09 17:21:05 +0200

    Plugin Finished without permissions

--- src/main/java/me/therandomgamer/serverlimitselector/GUIHandler.java
+++ src/main/java/me/therandomgamer/serverlimitselector/GUIHandler.java
@@ -4,9 +4,13 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -20,8 +24,8 @@ import java.util.Arrays;
  */
 public class GUIHandler implements Listener {
 
-    @EventHandler
-    public void onItemClick(PlayerInteractEvent e) {
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onItemInteract(PlayerInteractEvent e) {
         if ((e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) && e.getItem().getType() == Material.COMPASS) {
             FileConfiguration c = Main.getMain().getConfig();
             int size = (((c.getConfigurationSection("servers").getKeys(false).size() / 9) + 1) * 9);
@@ -39,9 +43,38 @@ public class GUIHandler implements Listener {
                 is.setItemMeta(ismeta);
                 gui.addItem(is);
             }
+
             e.getPlayer().openInventory(gui);
         }
     }
 
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onItemClick(InventoryClickEvent e) {
+        FileConfiguration c = Main.getMain().getConfig();
+        if (e.getInventory().getName() == c.getString("selector.name")) {
+            e.setCancelled(true);
+            ItemStack is = e.getCursor();
+            String name = is.getItemMeta().getDisplayName();
 
+            for (String s : c.getConfigurationSection("servers").getKeys(false)) {
+                if (name.equals(c.getString("servers." + s + ".itemname"))) {
+                    BungeeChannelConnection bcc = BungeeChannelConnection.getInstance();
+                    int onlinePlayers = bcc.getServerPlayers(s);
+                    int maxOnlinePlayers = c.getInt("servers." + s + ".maxplayers");
+                    if (onlinePlayers < maxOnlinePlayers) {
+                        bcc.connectPlayer((Player) e.getWhoClicked(), s);
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOW)
+    public void moveEvent(InventoryMoveItemEvent e) {
+        String name = Main.getMain().getConfig().getString("selector.name");
+        if (e.getSource().getName().equals(name) || e.getDestination().getName().equals(name)) {
+            e.setCancelled(true);
+        }
+    }
 }
